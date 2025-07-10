29723fa92f8b4a0096f071dc5bdfed3dacf582ea
Adam Schaal <amschaal@gmail.com>
2025-07-10 11:14:35 -0700

    Add function to Schema utility for converting coreomics schemas.

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 const Schema = {}
 Schema.getVariablesOfType = function (schema, t) {
   if (!schema || !schema.order || !schema.properties) {
@@ -20,4 +21,22 @@ Schema.getTableSchema = function (schema, tableVariable) {
 Schema.getTableSchemas = function (schema) {
   return Schema.getTables(schema).map(v => ({ table: v, schema: Schema.getTableSchema(schema, v) }))
 }
+// Take a coreomics style schema, and convert it to a standard jsonschema, replacing 'table' type with 'array' type where necessary
+Schema.convertToJsonschema = function (coreomicsSchema) {
+  const schema = _.cloneDeep(coreomicsSchema)
+  schema['type'] = 'object'
+  // Make changes to schema
+  if ('properties' in schema) {
+    Object.keys(schema.properties).forEach((k, i) => {
+      if (schema.properties[k].type && schema.properties[k].type.toLowerCase() === 'table') {
+        const listProp = {'type': 'array', 'items': schema['properties'][k]['schema']}
+        delete schema['properties'][k]['schema']
+        schema['properties'][k] = listProp
+        schema['properties'][k]['items']['type'] = 'object'
+      }
+    })
+  }
+  return schema
+}
+
 export default Schema
